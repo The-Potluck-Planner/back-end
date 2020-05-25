fae8a16610641a59927e152d5fea503b8a4788e9
Rachele Edwards <dot.com1472@gmail.com>
2020-05-25 11:16:47 -0600

    updated knexfile for useNullAsDefault, production and pgConnection

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,10 @@
-// Update with your config settings.
+// const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@locathost/api/ADD_HERE"
 
 module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
       filename: './data/potluck.db3'
     },
@@ -16,18 +17,17 @@ module.exports = {
   },
 
   // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
+  //   client: 'pg',
+  //   connection: pgConnection,
   //   pool: {
   //     min: 2,
   //     max: 10
   //   },
   //   migrations: {
-  //     tableName: 'knex_migrations'
+  //     directory: "./data/migrations"
+  //   },
+  //   seeds: {
+  //     directory: "./data/seeds"
   //   }
   // }
 
