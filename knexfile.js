657db5a83cb9fb963ba46d9d310b954960b26902
Rachele Edwards <dot.com1472@gmail.com>
2020-05-26 10:32:08 -0600

    added credits and api link to README, added production in knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-// const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@locathost/api/ADD_HERE"
+const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@locathost/api/events"
 
 module.exports = {
 
@@ -16,19 +16,19 @@ module.exports = {
     }
   },
 
-  // production: {
-  //   client: 'pg',
-  //   connection: pgConnection,
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     directory: "./data/migrations"
-  //   },
-  //   seeds: {
-  //     directory: "./data/seeds"
-  //   }
-  // }
+  production: {
+    client: 'pg',
+    connection: pgConnection,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
 
 };
