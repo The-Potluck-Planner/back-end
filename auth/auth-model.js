72944812c654f625141a07bc84bde5a4178af973
Rachele Edwards <dot.com1472@gmail.com>
2020-05-26 15:01:00 -0600

    changed add function

--- auth/auth-model.js
+++ auth/auth-model.js
@@ -6,19 +6,19 @@ module.exports = {
     findBy,
 }
 
-async function add(user) {
-    // return db("users").insert(user)
-    // .then(show => {
-    //     return findByID(show[0])
-    // })
-    const id = await db("users").insert(user)
-    return findByID(id)
-}
-
 function findByID(id) {
     return db("users").where({id}).first()
 }
 
 function findBy(filter){
     return db("users").where(filter)
+}
+
+async function add(user) {
+    // return db("users").insert(user)
+    // .then(show => {
+    //     return findByID(show[0])
+    // })
+    const [newUser] = await db("users").insert(user, "*")
+    return newUser
 }
