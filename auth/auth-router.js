d586eea5d742cab3dc8301afb302154013258af7
Rachele Edwards <dot.com1472@gmail.com>
2020-05-26 08:52:07 -0600

    finished login router--starting events router then testing in Postman

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -39,11 +39,24 @@ router.post('/login', (req, res) => {
 
     if(isValidLogin(req.body)) {
         Users.findBy({username: username})
-        .then()
+        .then(([user]) => {
+            if(user && bcrypt.compareSync(password, user.password)){
+                const token = generateToken(user)
+
+                res.status(200).json({
+                    message: `Welcome, ${user.name}`,
+                    token: token
+                })
+            }else {
+                res.status(401).json({
+                    message: "Invalid username or password"
+                })
+            }
+        })
         .catch(err => {
             console.log({err})
             res.status(500).json({
-                message: `There was an error: `
+                message: `There was an error: ${err.message}`
             })
         })
     }else {
