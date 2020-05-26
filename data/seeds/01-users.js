bbb1505a0703b272fa7e05ffaab4b2e01af5b0d7
Rachele Edwards <dot.com1472@gmail.com>
2020-05-26 09:58:47 -0600

    created user seed with hashed password--test passes

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -3,7 +3,7 @@ exports.seed = function(knex) {
   return knex('users').del()
     .then(function () {
       return knex('users').insert([
-        {id: 1, name: "Rachele Edwards", username: "berachele", password: "password"},
+        {id: 1, name: "Rachele", username: "lambda", password: "$2a$10$atZ61ga6YEgot.aEqJhFReD5xg34.QeZiG8oCq1zCObQy8PRW9AK2"},
       ]);
     });
 };
