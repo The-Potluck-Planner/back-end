baed68849aa0e0da802777fca70b25864d9fc2e0
Rachele Edwards <dot.com1472@gmail.com>
2020-05-29 10:50:45 -0600

    passing register tests

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -1,8 +1,40 @@
 const supertest = require('supertest')
 const server = require('../api/server')
+const db = require('../data/db-config')
+
+beforeEach(() => {
+    return db.migrate.rollback()
+    .then(() => db.migrate.latest())
+    .then(() => db.seed.run())
+})
 
 describe('AUTH', () => {
     it('can run test', () => {
         expect(true).toBeTruthy()
+    })
+
+    describe('POST Register', () => {
+        it('Returns status 201', () => {
+            return supertest(server)
+            .post('/auth/register')
+            .send({name: "Derek", username: "derknhood", password: "school"})
+            .then(res => {
+                expect(res.status).toEqual(201)
+            })
+        })
+
+        it('Should return that user', () => {
+            return supertest(server)
+            .post('/auth/register')
+            .send({name: "Derek", username: "derknhood", password: "school"})
+            .then(res => {
+                expect(Array.isArray).toHaveLength(1)
+            })
+        })
+    })
+
+    describe('POST Login', () => {
+        it.todo('Returns status 200')
+        it.todo('can see users name')
     })
 })
